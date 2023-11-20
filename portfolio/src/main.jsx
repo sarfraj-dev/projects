56b36742213d2463eb05f48e5f4f854a1386ea70
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2023-11-21 02:21:42 +0530

    about us content added

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/Header'
 import Home from './components/Home'
 import './index.css'
+import About from './components/About'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Header /> 
   <Home />
+  <About />
   </React.StrictMode>,
 )
